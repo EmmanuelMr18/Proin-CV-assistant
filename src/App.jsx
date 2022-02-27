4d8461a212b3dfa2fdcd5beeb9a5d7bdb3190db6
EmmanuelMr18 <emm_mr@hotmail.com>
2022-02-27 12:54:41 -0600

    feat: base project created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { InitialForm } from './components/InitialForm'
+
+function App() {
+  return (
+    <div>
+      <p>Este es mi texto</p>
+      <InitialForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/InitialForm.js
@@ -0,0 +1,5 @@
+import h from "react-hyperscript";
+
+export function InitialForm(){
+    return h('div', {}, `este es mi div`)
+}
