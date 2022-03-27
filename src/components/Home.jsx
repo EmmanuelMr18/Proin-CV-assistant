1d96470f2a347e37659f84608621635b364d8f56
EmmanuelMr18 <emm_mr@hotmail.com>
2022-03-26 23:00:53 -0600

    feat(home): initial design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,31 +2,58 @@ import { Button } from '@mui/material';
 import { Fragment, useContext, useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../main';
+import basicDesign from '../assets/templates/basicDesign.png';
+import squareDesign from '../assets/templates/squareDesign.png';
 import { InitialForm } from './InitialForm';
 import { Navbar } from './Navbar';
 
 export function Home() {
     const user = useContext(UserContext);
+    const { userImg, name, description } = user;
     const navigate = useNavigate();
     return (
         <Fragment>
-            <div className="mw8 mt4 pa3 center">
-                <p>{`Bienvenido ${user.name}`}</p>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={() => navigate(`/fill-data`)}
-                >
-                    Editar mis datos
-                </Button>
+            <div className="mw9 mt4 pa3 center flex justify-between items-center">
+                <div className="user flex flex-wrap w-80">
+                    {userImg && (
+                        <div className="user__img">
+                            <img
+                                className="user__img__picture"
+                                src={userImg}
+                                alt={`${name}-picture`}
+                            />
+                        </div>
+                    )}
+                    <div className="pl4 flex flex-column justify-center w-80">
+                        <span className="db fw6 ">{`Bienvenido ${user.name}`}</span>
+                        <p className='mb0'>
+                            <span className="fw6">Descripción: </span>
+                            {user.description}
+                        </p>
+                    </div>
+                </div>
+                <div className="w-20">
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => navigate(`/fill-data`)}
+                    >
+                        Editar mis datos
+                    </Button>
+                </div>
             </div>
-            <div className="mw8 pv4 pa3 center">
-                <a href="/preview?design=basicDesign">
-                    <article>Diseño básico</article>
-                </a>
-                <a href="/preview?design=SquareDisign">
-                    <article>Diseño cuadrado</article>
-                </a>
+            <div className="mw9 pv4 pa3 center">
+                <h4 className="fw5 ">Diseños disponibles</h4>
+                <section className="designs">
+                    <a className='designs__item dim' href="/preview?design=basicDesign">
+                        <img className='designs__item__img' src={basicDesign} alt="BasicDesign" />
+                        {/* <article>Diseño básico</article> */}
+                    </a>
+                    <a className='designs__item dim' href="/preview?design=SquareDisign">
+                        <img className='designs__item__img' src={squareDesign} alt="BasicDesign" />
+                        {/* <article>Diseño básico</article> */}
+                    </a>
+                </section>
             </div>
         </Fragment>
     );
