b1b076f1f6ea4cf5e2795dd6305fd728bf345ac7
EmmanuelMr18 <emm_mr@hotmail.com>
2023-02-08 22:12:59 -0600

    refactor: use correct fallbacks in useUserData

--- src/hooks/useUserData.jsx
+++ src/hooks/useUserData.jsx
@@ -5,20 +5,16 @@ export function useUserData() {
   const [userData, setUserData] = useState(emptyuserData);
   useEffect(() => {
     const dataInLocalStorage = {
-      userImg: localStorage.getItem('userImg') || '',
-      name: localStorage.getItem('name') || '',
-      job: localStorage.getItem('job') || '',
-      languages: JSON.parse(localStorage.getItem('languages')) || [],
-      skills: JSON.parse(localStorage.getItem('skills')) || [],
-      description: localStorage.getItem('description') || '',
-      achievements: JSON.parse(localStorage.getItem('achievements')) || [''],
-      contacts: JSON.parse(localStorage.getItem('contacts')) || [''],
-      education: JSON.parse(localStorage.getItem('education')) || [
-        { institution: '', title: '', start: '', end: '' }
-      ],
-      experience: JSON.parse(localStorage.getItem('experience')) || [
-        { company: '', job: '', start: '', end: '', description: '' }
-      ]
+      userImg: localStorage.getItem('userImg') || emptyuserData.userImg,
+      name: localStorage.getItem('name') || emptyuserData.name,
+      job: localStorage.getItem('job') || emptyuserData.job,
+      languages: JSON.parse(localStorage.getItem('languages')) || emptyuserData.languages,
+      skills: JSON.parse(localStorage.getItem('skills')) || emptyuserData.skills,
+      description: localStorage.getItem('description') || emptyuserData.description,
+      achievements: JSON.parse(localStorage.getItem('achievements')) || emptyuserData.achievements,
+      contacts: JSON.parse(localStorage.getItem('contacts')) || emptyuserData.contacts,
+      education: JSON.parse(localStorage.getItem('education')) || emptyuserData.education,
+      experience: JSON.parse(localStorage.getItem('experience')) || emptyuserData.experience
     };
     setUserData(dataInLocalStorage);
   }, []);
