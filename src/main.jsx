fd95de247d60d355e408813234446884d5b389c9
EmmanuelMr18 <emm_mr@hotmail.com>
2023-02-12 12:45:07 -0600

    fix(dev): calling ReactDOMClient.createRoot() with the hot reload

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,23 @@
 import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 import './styles/index.scss';
 import AppRouter from './components/AppRouter';
 import { theme } from './styles/theme';
 
 export const UserContext = React.createContext({});
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <AppRouter />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+let container = null;
+document.addEventListener('DOMContentLoaded', function () {
+  if (!container) {
+    container = document.getElementById('root');
+    const root = createRoot(container);
+    root.render(
+      <React.StrictMode>
+        <ThemeProvider theme={theme}>
+          <AppRouter />
+        </ThemeProvider>
+      </React.StrictMode>
+    );
+  }
+});
