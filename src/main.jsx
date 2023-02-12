5d637b9deeb6c9168037692ee24aa70d08d6982b
EmmanuelMr18 <emm_mr@hotmail.com>
2023-02-11 23:11:34 -0600

    build: update react to v18

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from '@emotion/react';
-import { createTheme } from '@mui/material';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './styles/index.scss';
 import AppRouter from './components/AppRouter';
@@ -9,11 +8,12 @@ import { theme } from './styles/theme';
 
 export const UserContext = React.createContext({});
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <AppRouter />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
