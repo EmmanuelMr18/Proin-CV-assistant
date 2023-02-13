44b0c2996df027383adeb327af1e18c1e1866064
EmmanuelMr18 <emm_mr@hotmail.com>
2023-02-13 13:06:45 -0600

    feat: give functionalities to actions menu

--- src/components/home/ActionsMenu.jsx
+++ src/components/home/ActionsMenu.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Button, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { exportObj, importText } from '../../data/utlis';
+import { UserContext } from '../../main';
+import { emptyuserData } from '../../models/user';
 
 export function ActionsMenu() {
+  const user = useContext(UserContext);
   const [actionsEl, setActionsEl] = useState(null);
 
   function handleActionsBtn(event) {
@@ -13,6 +17,18 @@ export function ActionsMenu() {
   }
   function handleOnClose() {
     setActionsEl(null);
+  }
+  async function importData(event) {
+    const dataImported = await importText(event);
+    const dataObj = JSON.parse(dataImported);
+    user.update({ ...dataObj });
+  }
+  function exportData() {
+    const filename = `${user.data.name} proinCV.json`;
+    exportObj(user.data, filename);
+  }
+  function deleteData() {
+    user.update({ ...emptyuserData });
   }
   return (
     <>
@@ -25,25 +41,33 @@ export function ActionsMenu() {
         Más acciones
       </Button>
       <Menu id="basic-menu" anchorEl={actionsEl} open={Boolean(actionsEl)} onClose={handleOnClose}>
-        <MenuItem onClick={handleOnClose}>
-          <ListItemIcon>
-            <FileUploadOutlinedIcon />
-          </ListItemIcon>
-          <ListItemText>Cargar datos</ListItemText>
-        </MenuItem>
-        <MenuItem divider onClick={handleOnClose}>
+        <label htmlFor="import-data">
+          <MenuItem onClick={handleOnClose}>
+            <ListItemIcon>
+              <FileUploadOutlinedIcon />
+            </ListItemIcon>
+            <ListItemText>Cargar datos</ListItemText>
+          </MenuItem>
+        </label>
+        <MenuItem divider onClick={exportData}>
           <ListItemIcon>
             <FileDownloadOutlinedIcon />
           </ListItemIcon>
           <ListItemText>Descargar datos</ListItemText>
         </MenuItem>
-        <MenuItem onClick={handleOnClose}>
+        <MenuItem onClick={deleteData}>
           <ListItemIcon>
             <DeleteOutlineOutlinedIcon />
           </ListItemIcon>
           <ListItemText>Borrar datos</ListItemText>
         </MenuItem>
       </Menu>
+      <input
+        className="home__header__actions__hidden"
+        type="file"
+        id="import-data"
+        onChange={importData}
+      />
     </>
   );
 }
